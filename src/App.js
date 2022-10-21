ed44c2e8637290d8b67ad4ec9567b8a2662a7c6b
Debbie <asamoahjaeldeborah@gmail.com>
2022-10-21 16:34:09 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import TailwindAndStyledComponentsNavbar from "./tailwind-styled/navbar";
+// import NavbarWithButtons from "./tailwindcss/navbar/index.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <NavbarWithButtons /> */}
+      <TailwindAndStyledComponentsNavbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
   );
 }
 
